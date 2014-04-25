b549bbc9c1744fb8c50a2f577d239b3f9b8ee8a8
Tamás Gulácsi <gt-tgulacsi-desktop@gthomas.eu>
2014-04-25 13:32:17 +0200

    merge

--- honlap/build.sh
+++ honlap/build.sh
@@ -11,5 +11,5 @@ if [ $(docker images | grep tgulacsi/honlap | wc -l) -le 0 ]; then
 	echo 'NO tgulacsi/honlap, building it'
 	docker build -t tgulacsi/honlap $(dirname $0)/
 fi
-echo docker run -t -i -v /tmp/honlap:/tmp/honlap tgulacsi/honlap /bin/bash -c 'cd /tmp/honlap && python2.7 ./gen.py'
-docker run -t -i -v /tmp/honlap:/tmp/honlap tgulacsi/honlap /bin/bash -c 'cd /tmp/honlap && python2.7 ./gen.py'
+echo docker run -t -i -v /tmp/honlap:/tmp/honlap tgulacsi/honlap /bin/bash -c 'cd /tmp/honlap && ./gen.py'
+docker run -t -i -v /tmp/honlap:/tmp/honlap tgulacsi/honlap /bin/bash -c 'cd /tmp/honlap && ./gen.py'
