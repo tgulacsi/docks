dde1adee6acebda017764f261950828c0e3bc49e
Tamás Gulácsi <gt-tgulacsi-desktop@gthomas.eu>
2014-04-25 11:51:19 +0200

    honlap builder script

---
+++ honlap/build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -e
+cd /tmp
+if [ ! -d honlap/.git ]; then
+	rm -rf honlap
+	git clone gitolite@git.gthomas.eu:unosoft/honlap
+else
+	cd honlap
+	git pull
+fi
+if [ $(docker images | grep tgulacsi/honlap | wc -l) -le 0 ]; then
+	echo 'NO tgulacsi/honlap, building it'
+	docker build -t tgulacsi/honlap $(dirname $0)/
+fi
+echo docker run -t -i -v /tmp/honlap:/tmp/honlap tgulacsi/honlap /bin/bash -c 'cd /tmp/honlap && python2.7 ./gen.py'
+docker run -t -i -v /tmp/honlap:/tmp/honlap tgulacsi/honlap /bin/bash -c 'cd /tmp/honlap && python2.7 ./gen.py'
